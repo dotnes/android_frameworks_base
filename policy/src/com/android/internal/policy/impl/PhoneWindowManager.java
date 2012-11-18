937611e2e0e71f299af4782a8aa4880a6610d343
JBirdVegas <jbirdvegas@gmail.com>
2012-11-18 07:23:30 -0600

    BootMessage: show more info in boot dialog
    Tell the users what package we are running dexOpt on currently
    
    PS4: made all necessary changes to make this work.
    
    Change-Id: I665ab3847ec1846e40915a1fae21c43e2e7ccf6d

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -237,8 +237,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 KeyEvent.KEYCODE_CALCULATOR, Intent.CATEGORY_APP_CALCULATOR);
     }
 
-    private final DeviceKeyHandler mDeviceKeyHandler;
-
     /**
      * Lock protecting internal state.  Must not call out into window
      * manager with lock held.  (This lock will be acquired in places
@@ -734,10 +732,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
     MyOrientationListener mOrientationListener;
-
-    public PhoneWindowManager(IDeviceHandler device) {
-        mDeviceKeyHandler = (device != null) ? device.getDeviceKeyHandler() : null;
-    }
 
     IStatusBarService getStatusBarService() {
         synchronized (mServiceAquireLock) {
@@ -2604,18 +2598,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         if (keyCode == KeyEvent.KEYCODE_BACK && !down) {
             mHandler.removeCallbacks(mKillTask);
-        }
-
-        // Specific device key handling
-        if (mDeviceKeyHandler != null) {
-            try {
-                // The device only should consume known keys.
-                if (mDeviceKeyHandler.handleKeyEvent(event)) {
-                    return -1;
-                }
-            } catch (Exception e) {
-                Slog.w(TAG, "Could not dispatch event to device key handler", e);
-            }
         }
 
         // First we always handle the home key here, so applications
@@ -5474,7 +5456,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     // debugging 'Android is upgrading...' ProgressDialog
     final boolean DEBUG_BOOTMSG = false;
-
     // this method is called to create, if needed, and update boot ProgressDialog
     // see @link frameworks/base/services/java/com/android/server/pm/
     //              PackageManagerService.performBootDexOpt()
