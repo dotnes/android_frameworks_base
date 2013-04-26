412e24acee2b8a67fe75527684398823d9fb63b7
andrew.boren <andrew.boren@gmail.com>
2013-04-25 21:24:49 -0700

    fwbs: Port lockscreen styles
    * Includes: ICS Multiwave, GB Sliding tab, Eclair rotary dial & Optimus 4.
    
    PS2: Cleaned it up
    
    Change-Id: I7e6c7d189a6a843395a037c502e4d1139351abca

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2615,6 +2615,22 @@ public final class Settings {
         public static final String LOCKSCREEN_DISABLED = "lockscreen.disabled";
 
         /**
+         * Which lockscreen style to use. The value is an integer.
+         * ( 0 - GlowPadView    : JB     )
+         * ( 1 - MultiwaveView  : ICS    )
+         * ( 2 - Tabs           : GB     )
+         * ( 3 - Rotary         : Eclair )
+         * @hide
+         */
+        public static final String LOCKSCREEN_STYLE = "lockscreen_style";
+
+        /**
+         * Whether to use optimus4 background color as lockscreen background color.
+         * @hide
+         */
+        public static final String LOCKSCREEN_USEOP4COLOR = "lockscreen_useop4color";
+
+        /**
          * Stores values for custom lockscreen targets
          * @hide
          */
@@ -5089,6 +5105,31 @@ public final class Settings {
          * @hide
          */
         public static final String NOTIFICATIONS_BEHAVIOUR = "notifications_behaviour";
+
+
+        /**
+         * Sets bg color of optimus lockscreen bg
+         * @hide
+         */
+	public static final String CIRCLES_LOCK_BG_COLOR = "circles_lock_bg_color";
+
+        /**
+         * Sets ring color of optimus lockscreen
+         * @hide
+         */
+        public static final String CIRCLES_LOCK_RING_COLOR = "circles_lock_ring_color";
+
+        /**
+         * Sets halo color of optimus lockscreen
+         * @hide
+         */
+        public static final String CIRCLES_LOCK_HALO_COLOR = "circles_lock_halo_color";
+
+        /**
+         * Sets wave color of optimus lockscreen
+         * @hide
+         */
+        public static final String CIRCLES_LOCK_WAVE_COLOR = "circles_lock_wave_color";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
