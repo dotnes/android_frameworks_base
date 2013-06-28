0fca14411620d675cce6c2ea6d37ad40ea24edc9
itsmikeramsay <ronaldramsayii@gmail.com>
2013-06-27 23:59:59 -0400

    Nexus 7 animations
    Change-Id: I1ce56a7e5d575f007dda559f7cb975221c3b651c

---
+++ packages/SystemUI/src/com/android/systemui/quicksettings/ToggleHoloBamTile.java
@@ -0,0 +1,77 @@
+package com.android.systemui.quicksettings;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+
+import com.android.systemui.R;
+import com.android.systemui.statusbar.phone.QuickSettingsContainerView;
+import com.android.systemui.statusbar.phone.QuickSettingsController;
+
+public class ToggleHoloBamTile extends QuickSettingsTile {
+
+    public ToggleHoloBamTile(Context context,
+            QuickSettingsController qsc, Handler handler) {
+        super(context, qsc);
+
+        mLabel = context.getString(R.string.quick_settings_holobam_label);
+        mOnClick = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Settings.Secure.putInt(mContext.getContentResolver(),
+                        Settings.Secure.UI_INVERTED_MODE, !getUiInvertedMode() ? 2 : 1);
+                if (isFlipTilesEnabled()) {
+                    flipTile(0);
+                }
+            }
+        };
+
+        mOnLongClick = new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                startSettingsActivity(Settings.ACTION_SETTINGS);
+                return true;
+            }
+        };
+        qsc.registerObservedContent(Settings.Secure.getUriFor(Settings.Secure.UI_INVERTED_MODE)
+                , this);
+    }
+
+    @Override
+    public void updateResources() {
+        updateTile();
+        updateQuickSettings();
+    }
+
+    private synchronized void updateTile() {
+        if(getUiInvertedMode()){
+            mDrawable = R.drawable.ic_qs_jb_dark_on;
+            mLabel = mContext.getString(R.string.quick_settings_holobam_on_label);
+        }else{
+            mDrawable = R.drawable.ic_qs_jb_dark_off;
+            mLabel = mContext.getString(R.string.quick_settings_holobam_off_label);
+        }
+    }
+
+    @Override
+    void onPostCreate() {
+        updateTile();
+        super.onPostCreate();
+    }
+
+    private boolean getUiInvertedMode() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.UI_INVERTED_MODE, 1) == 2;
+    }
+
+    @Override
+    public void onChangeUri(ContentResolver resolver, Uri uri) {
+        updateResources();
+    }
+}
