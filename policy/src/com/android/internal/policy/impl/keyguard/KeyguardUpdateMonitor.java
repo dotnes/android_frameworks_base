fdcff6e20990347943e96f77ab34f15b7756974d
Rakesh Pallerla <rakesh@codeaurora.org>
2013-07-08 18:49:16 +0530

    Telephony: Add DETECTED state and PIN/PUK retry count.
    1. Add App state DETECTED in the IccCardConstants.State enum.
       Modified logic for allowing PIN cancel based on number of
       cards configured/activated instead of number of cards present.
    
    2. Add method in ITelephonyMSim.aidl to retrieve PIN1RetryCount.
       Display Attempts remaining in PIN/PUK lockscreen UI.
    
    3. Clean up KeyguardUpdateMonitorCallback and MSimCarrierText by
       removing Initialize method from Callback.
    
    Change-Id: I89c9825b35257c1cca5946bf0cbffedb07d0015b
    CRs-Fixed: 501887, 504086, 508283, 510400, 513293
    
    Conflicts:
    
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardUpdateMonitor.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardUpdateMonitorCallback.java
    	policy/src/com/android/internal/policy/impl/keyguard/msim/MSimCarrierText.java
    	policy/src/com/android/internal/policy/impl/keyguard/msim/MSimKeyguardSimPinView.java
    	policy/src/com/android/internal/policy/impl/keyguard/msim/MSimKeyguardSimPukView.java

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardUpdateMonitor.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardUpdateMonitor.java
@@ -717,13 +717,15 @@ public class KeyguardUpdateMonitor {
     private void handleSimStateChange(SimArgs simArgs) {
         final IccCardConstants.State state = simArgs.simState;
 
-        if (DEBUG) {
-            Log.d(TAG, "handleSimStateChange: intentValue = " + simArgs + " "
-                    + "state resolved to " + state.toString());
-        }
+        Log.d(TAG, "handleSimStateChange: intentValue = " + simArgs + " "
+                + "state resolved to " + state.toString() + " "
+                + "subscription =" + subscription);
 
-        if (state != IccCardConstants.State.UNKNOWN && state != mSimState) {
-            mSimState = state;
+        if (state != IccCardConstants.State.UNKNOWN && state != mSimState[subscription]) {
+            if (DEBUG_SIM_STATES) Log.v(TAG, "dispatching state: " + state
+                    + "subscription: " + subscription);
+
+            mSimState[subscription] = state;
             for (int i = 0; i < mCallbacks.size(); i++) {
                 KeyguardUpdateMonitorCallback cb = mCallbacks.get(i).get();
                 if (cb != null) {
@@ -903,7 +905,13 @@ public class KeyguardUpdateMonitor {
      * through mHandler, this *must* be called from the UI thread.
      */
     public void reportSimUnlocked() {
-        handleSimStateChange(new SimArgs(IccCardConstants.State.READY));
+        reportSimUnlocked(MSimTelephonyManager.getDefault().getDefaultSubscription());
+    }
+
+    public void reportSimUnlocked(int subscription) {
+        if (DEBUG) Log.d(TAG, "reportSimUnlocked(" + subscription + ")");
+        mSimState[subscription] = IccCardConstants.State.READY;
+        handleSimStateChange(new SimArgs(mSimState[subscription], subscription));
     }
 
     public CharSequence getTelephonyPlmn() {
