81443d8a1e7d6736415ae9c973c0c8b243ffc51b
iGio90 <giovanni.rocca.90@gmail.com>
2013-08-14 02:16:05 +0200

    Additiona things 1

--- services/java/com/android/server/LockSettingsService.java
+++ services/java/com/android/server/LockSettingsService.java
@@ -14,29 +14,32 @@
  * limitations under the License.
  */
 
-package com.android.internal.widget;
+package com.android.server;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.UserInfo;
+
+import static android.content.Context.USER_SERVICE;
+import static android.Manifest.permission.READ_PROFILE;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.gesture.Gesture;
-import android.gesture.GestureLibraries;
-import android.gesture.GestureLibrary;
-import android.gesture.Prediction;
-import android.gesture.GestureStore;
 import android.os.Binder;
 import android.os.Environment;
 import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import android.os.UserManager;
 import android.provider.Settings;
 import android.provider.Settings.Secure;
+import android.provider.Settings.SettingNotFoundException;
 import android.text.TextUtils;
 import android.util.Slog;
 
+import com.android.internal.widget.ILockSettings;
 import com.android.internal.widget.LockPatternUtils;
 
 import java.io.File;
@@ -44,7 +47,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Arrays;
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Keeps the lock pattern/password data and related settings for each user.
@@ -69,9 +72,6 @@ public class LockSettingsService extends ILockSettings.Stub {
     private static final String SYSTEM_DIRECTORY = "/system/";
     private static final String LOCK_PATTERN_FILE = "gesture.key";
     private static final String LOCK_PASSWORD_FILE = "password.key";
-    private static final String LOCK_GESTURE_FILE = "lock_gesture.key";
-
-    private static final String LOCK_GESTURE_NAME = "lock_gesture";
 
     private final Context mContext;
 
@@ -87,23 +87,59 @@ public class LockSettingsService extends ILockSettings.Stub {
 
     private void migrateOldData() {
         try {
-            if (getString("migrated", null, 0) != null) {
-                // Already migrated
-                return;
+            // These Settings moved before multi-user was enabled, so we only have to do it for the
+            // root user.
+            if (getString("migrated", null, 0) == null) {
+                final ContentResolver cr = mContext.getContentResolver();
+                for (String validSetting : VALID_SETTINGS) {
+                    String value = Settings.Secure.getString(cr, validSetting);
+                    if (value != null) {
+                        setString(validSetting, value, 0);
+                    }
+                }
+                // No need to move the password / pattern files. They're already in the right place.
+                setString("migrated", "true", 0);
+                Slog.i(TAG, "Migrated lock settings to new location");
             }
 
-            final ContentResolver cr = mContext.getContentResolver();
-            for (String validSetting : VALID_SETTINGS) {
-                String value = Settings.Secure.getString(cr, validSetting);
-                if (value != null) {
-                    setString(validSetting, value, 0);
+            // These Settings changed after multi-user was enabled, hence need to be moved per user.
+            if (getString("migrated_user_specific", null, 0) == null) {
+                final UserManager um = (UserManager) mContext.getSystemService(USER_SERVICE);
+                final ContentResolver cr = mContext.getContentResolver();
+                List<UserInfo> users = um.getUsers();
+                for (int user = 0; user < users.size(); user++) {
+                    // Migrate owner info
+                    final int userId = users.get(user).id;
+                    final String OWNER_INFO = Secure.LOCK_SCREEN_OWNER_INFO;
+                    String ownerInfo = Settings.Secure.getStringForUser(cr, OWNER_INFO, userId);
+                    if (ownerInfo != null) {
+                        setString(OWNER_INFO, ownerInfo, userId);
+                        Settings.Secure.putStringForUser(cr, ownerInfo, "", userId);
+                    }
+
+                    // Migrate owner info enabled.  Note there was a bug where older platforms only
+                    // stored this value if the checkbox was toggled at least once. The code detects
+                    // this case by handling the exception.
+                    final String OWNER_INFO_ENABLED = Secure.LOCK_SCREEN_OWNER_INFO_ENABLED;
+                    boolean enabled;
+                    try {
+                        int ivalue = Settings.Secure.getIntForUser(cr, OWNER_INFO_ENABLED, userId);
+                        enabled = ivalue != 0;
+                        setLong(OWNER_INFO_ENABLED, enabled ? 1 : 0, userId);
+                    } catch (SettingNotFoundException e) {
+                        // Setting was never stored. Store it if the string is not empty.
+                        if (!TextUtils.isEmpty(ownerInfo)) {
+                            setLong(OWNER_INFO_ENABLED, 1, userId);
+                        }
+                    }
+                    Settings.Secure.putIntForUser(cr, OWNER_INFO_ENABLED, 0, userId);
                 }
+                // No need to move the password / pattern files. They're already in the right place.
+                setString("migrated_user_specific", "true", 0);
+                Slog.i(TAG, "Migrated per-user lock settings to new location");
             }
-            // No need to move the password / pattern files. They're already in the right place.
-            setString("migrated", "true", 0);
-            Slog.i(TAG, "Migrated lock settings to new location");
         } catch (RemoteException re) {
-            Slog.e(TAG, "Unable to migrate old data");
+            Slog.e(TAG, "Unable to migrate old data", re);
         }
     }
 
@@ -123,12 +159,16 @@ public class LockSettingsService extends ILockSettings.Stub {
         }
     }
 
-    private static final void checkReadPermission(int userId) {
+    private final void checkReadPermission(String requestedKey, int userId) {
         final int callingUid = Binder.getCallingUid();
-        if (UserHandle.getAppId(callingUid) != android.os.Process.SYSTEM_UID
-                && UserHandle.getUserId(callingUid) != userId) {
-            throw new SecurityException("uid=" + callingUid
-                    + " not authorized to read settings of user " + userId);
+        for (int i = 0; i < READ_PROFILE_PROTECTED_SETTINGS.length; i++) {
+            String key = READ_PROFILE_PROTECTED_SETTINGS[i];
+            if (key.equals(requestedKey) && mContext.checkCallingOrSelfPermission(READ_PROFILE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                throw new SecurityException("uid=" + callingUid
+                        + " needs permission " + READ_PROFILE + " to read "
+                        + requestedKey + " for user " + userId);
+            }
         }
     }
 
@@ -155,7 +195,7 @@ public class LockSettingsService extends ILockSettings.Stub {
 
     @Override
     public boolean getBoolean(String key, boolean defaultValue, int userId) throws RemoteException {
-        //checkReadPermission(userId);
+        checkReadPermission(key, userId);
 
         String value = readFromDb(key, null, userId);
         return TextUtils.isEmpty(value) ?
@@ -164,7 +204,7 @@ public class LockSettingsService extends ILockSettings.Stub {
 
     @Override
     public long getLong(String key, long defaultValue, int userId) throws RemoteException {
-        //checkReadPermission(userId);
+        checkReadPermission(key, userId);
 
         String value = readFromDb(key, null, userId);
         return TextUtils.isEmpty(value) ? defaultValue : Long.parseLong(value);
@@ -172,7 +212,7 @@ public class LockSettingsService extends ILockSettings.Stub {
 
     @Override
     public String getString(String key, String defaultValue, int userId) throws RemoteException {
-        //checkReadPermission(userId);
+        checkReadPermission(key, userId);
 
         return readFromDb(key, defaultValue, userId);
     }
@@ -213,25 +253,6 @@ public class LockSettingsService extends ILockSettings.Stub {
         }
     }
 
-    private String getLockGestureFilename(int userId) {
-        return getLockGestureFilename(userId, isDefaultSize(userId));
-    }
-
-    private String getLockGestureFilename(int userId, boolean defaultSize) {
-        String dataSystemDirectory =
-                android.os.Environment.getDataDirectory().getAbsolutePath() +
-                SYSTEM_DIRECTORY;
-        String patternFile = LOCK_GESTURE_FILE;
-
-        if (userId == 0) {
-            // Leave it in the same place for user 0
-            return dataSystemDirectory + patternFile;
-        } else {
-            return  new File(Environment.getUserSystemDirectory(userId), patternFile)
-                    .getAbsolutePath();
-        }
-    }
-
     private String getLockPasswordFilename(int userId) {
         String dataSystemDirectory =
                 android.os.Environment.getDataDirectory().getAbsolutePath() +
@@ -257,13 +278,6 @@ public class LockSettingsService extends ILockSettings.Stub {
         // Do we need a permissions check here?
 
         return new File(getLockPatternFilename(userId)).length() > 0;
-    }
-
-    @Override
-    public boolean haveGesture(int userId) throws RemoteException {
-        // Do we need a permissions check here?
-
-        return new File(getLockGestureFilename(userId)).length() > 0;
     }
 
     @Override
@@ -296,46 +310,6 @@ public class LockSettingsService extends ILockSettings.Stub {
             Slog.e(TAG, "Cannot read file " + ioe);
             return true;
         }
-    }
-
-    @Override
-    public void setLockGesture(Gesture gesture, int userId) throws RemoteException {
-        checkWritePermission(userId);
-        if (gesture == null)
-            return;
-
-        File storeFile = new File(getLockGestureFilename(userId));
-        GestureLibrary store = GestureLibraries.fromFile(storeFile);
-
-        store.load();
-        if (store.getGestures(LOCK_GESTURE_NAME) != null) {
-            store.removeEntry(LOCK_GESTURE_NAME);
-        }
-
-        store.addGesture(LOCK_GESTURE_NAME, gesture);
-        store.save();
-    }
-
-    @Override
-    public boolean checkGesture(Gesture gesture, int userId) throws RemoteException {
-        checkPasswordReadPermission(userId);
-
-        File storeFile = new File(getLockGestureFilename(userId));
-        GestureLibrary store = GestureLibraries.fromFile(storeFile);
-        int minPredictionScore = mContext.getResources().getInteger(
-                com.android.internal.R.integer.min_gesture_prediction_score);
-        store.setOrientationStyle(GestureStore.ORIENTATION_SENSITIVE);
-        store.load();
-        ArrayList<Prediction> predictions = store.recognize(gesture);
-        if (predictions.size() > 0) {
-            Prediction prediction = predictions.get(0);
-            if (prediction.score > minPredictionScore) {
-                if (prediction.name.equals(LOCK_GESTURE_NAME)) {
-                    return true;
-                }
-            }
-        }
-        return false;
     }
 
     @Override
