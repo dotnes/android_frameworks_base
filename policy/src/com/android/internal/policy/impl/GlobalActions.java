78de44b888cd1ed68367fc4ee718ffe5afb034f3
iGio90 <giovanni.rocca.90@gmail.com>
2013-08-27 18:38:29 +0530

    Update GlobalActions
    Change-Id: I57aaa09860b31df867d6369739545d4f8e58dac1

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -196,11 +196,9 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             }
             mDialog.dismiss();
             mDialog = null;
-            mDialog = createDialog();
             // Show delayed, so that the dismiss of the previous dialog completes
             mHandler.sendEmptyMessage(MESSAGE_SHOW);
         } else {
-            mDialog = createDialog();
             handleShow();
         }
     }
@@ -219,6 +217,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private void handleShow() {
         awakenIfNecessary();
+        mDialog = createDialog();
         prepareDialog();
 
         WindowManager.LayoutParams attrs = mDialog.getWindow().getAttributes();
@@ -1393,7 +1392,6 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             case MESSAGE_DISMISS:
                 if (mDialog != null) {
                     mDialog.dismiss();
-                    mDialog = null;
                 }
                 break;
             case MESSAGE_REFRESH:
