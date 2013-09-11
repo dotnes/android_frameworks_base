07f7ccb80c5181244655a444d72970d40c45c17f
Danny Baumann <dannybaumann@web.de>
2013-09-11 15:09:46 +0200

    squash in 2 commits:
    * Freeze screen while changing expanded desktop mode.
    
    * Add Lock volume keys in silent mode [1/2]
    
    Change-Id: Idc3fc0a1b2031b56eca3b44d80d68856ac8a193a
    Signed-off-by: iGio90 <giovanni.rocca.90@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1964,6 +1964,12 @@ public final class Settings {
         public static final int VOLUME_OVERLAY_NONE = 3;
 
         /**
+         * Lock Volume Keys, Whether to lock ringer volume changes in silent mode.
+         * @hide
+         */
+        public static final String LOCK_VOLUME_KEYS = "lock_volume_keys";
+
+        /**
          * Volume Adjust Sounds Enable, This is the noise made when using volume hard buttons
          * Defaults to 1 - sounds enabled
          * @hide
