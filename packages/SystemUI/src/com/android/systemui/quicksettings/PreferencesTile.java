5c9bf715ebac3202995c0fe283fda7ae0c9e3aa7
iGio90 <giovanni.rocca.90@gmail.com>
2013-09-15 18:38:46 +0200

    Revert "Fix settings tile"
    This reverts commit 8a639c8b9b0776eac0cbfa2bfb66e3c033bddcbb.
    
    Change-Id: Ic8608fe255631a8c3d7c6aea764fbe54f276b0da

--- packages/SystemUI/src/com/android/systemui/quicksettings/PreferencesTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/PreferencesTile.java
@@ -1,9 +1,6 @@
 package com.android.systemui.quicksettings;
 
 import android.content.Context;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -13,8 +10,6 @@ import com.android.systemui.statusbar.phone.QuickSettingsController;
 
 public class PreferencesTile extends QuickSettingsTile{
 
-    private static final String SETTINGS_PKG = "com.android.settings";
-
     public PreferencesTile(Context context, LayoutInflater inflater,
             QuickSettingsContainerView container, QuickSettingsController qsc) {
         super(context, inflater, container, qsc);
@@ -23,16 +18,7 @@ public class PreferencesTile extends QuickSettingsTile{
 
             @Override
             public void onClick(View v) {
-                try {
-                    Intent intent = new Intent("android.intent.action.MAIN");
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.setComponent(new ComponentName(SETTINGS_PKG,
-                            SETTINGS_PKG + ".MainActivity"));
-                    mContext.startActivity(intent);
-                    mQsc.mBar.collapseAllPanels(true);
-                } catch(NullPointerException e) {
-                    // No intent found for activity component
-                }
+                startSettingsActivity(android.provider.Settings.ACTION_SETTINGS);
             }
         };
     }
