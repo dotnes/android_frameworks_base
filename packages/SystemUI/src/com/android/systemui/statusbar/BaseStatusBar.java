29c6ec3745cdc29b2b5973f793a8bbf723e53ebc
Giovanni Rocca <giovanni.rocca.90@gmail.com>
2013-10-16 15:14:44 +0200

    Disable pie if it's not enabled on settings

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -681,12 +681,10 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     private boolean showPie() {
-        boolean expanded = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.EXPANDED_DESKTOP_STATE, 0) == 1;
-        boolean navbarZero = Integer.parseInt(ExtendedPropertiesUtils
-                .getProperty("com.android.systemui.navbar.dpi", "100")) == 0;
+        boolean pie = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PIE_CONTROLS, 0) == 1;
 
-        return (expanded || navbarZero);
+        return (pie);
     }
 
     public void updatePieControls() {
