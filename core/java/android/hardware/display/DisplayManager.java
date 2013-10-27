f4cc060815382ee156536ceb78be256e6e862ceb
Giovanni Rocca <giovanni.rocca.90@gmail.com>
2013-10-27 02:04:25 +0200

    cleanup duplicates

--- core/java/android/hardware/display/DisplayManager.java
+++ core/java/android/hardware/display/DisplayManager.java
@@ -66,9 +66,6 @@ public final class DisplayManager {
     public static final String EXTRA_WIFI_DISPLAY_STATUS =
             "android.hardware.display.extra.WIFI_DISPLAY_STATUS";
 
-    public static final String ACTION_REMOTE_DISPLAY_STATUS_CHANGED =
-            "android.hardware.display.action.REMOTE_DISPLAY_STATUS_CHANGED";
-
     public static final String EXTRA_REMOTE_DISPLAY_STATUS =
             "android.hardware.display.extra.REMOTE_DISPLAY_STATUS";
 
