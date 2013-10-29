20a68f58529ef3a4d4dedce8445bbc2d8408baee
Giovanni Rocca <giovanni.rocca.90@gmail.com>
2013-10-30 00:50:56 +0100

    added missing HoloBam tile constants

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -29,11 +29,12 @@ public class QSConstants {
         public static final String TILE_USBTETHER = "toggleUsbTether";
         public static final String TILE_DESKTOPMODE = "toggleDesktopMode";
         public static final String TILE_VOLUME = "toggleVolume";
-     	 public static final String TILE_QUIETHOURS = "toggleQuietHours";
+        public static final String TILE_QUIETHOURS = "toggleQuietHours";
         public static final String TILE_HYBRID = "toggleHybrid";
         public static final String TILE_CAMERA = "toggleCamera";
         public static final String TILE_MUSIC = "toggleMusic";
         public static final String TILE_NETWORKADB = "toggleNetworkAdb";
+        public static final String TILE_HOLOBAM = "toggleHoloBam";
 
         public static final String TILE_DELIMITER = "|";
         public static ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
